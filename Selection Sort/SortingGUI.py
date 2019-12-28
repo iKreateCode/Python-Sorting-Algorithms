d26e12e49642c449c0f2940971da94afd7df4df2
emaz <emaz111@hotmail.com>
2019-12-28 16:18:45 +0000

    Added reset button and min index label

--- Selection Sort/SortingGUI.py
+++ Selection Sort/SortingGUI.py
@@ -21,18 +21,21 @@ def buttonEnabler():
 def arrayList(number):
     arrayListEntry.delete(0, END)
     numberArray.append(number)
-    e.config(text=str(numberArray))
+    valueArrayLabel.config(text=str(numberArray))
     buttonEnabler()
     print numberArray
 
 def randomiser():
     for i in range(10):
         numberArray.append(random.randint(1,100))
-        e.config(text=str(numberArray))
+        valueArrayLabel.config(text=str(numberArray))
         print numberArray
     buttonEnabler()
 
-
+def reset():
+    valueArrayLabel.configure(text="[Empty]")
+    minIndexLabel.configure(text="Minimum Index: none")
+    del numberArray[:]
 
 # Main selection sort function
 def sort(numberList):
@@ -41,16 +44,17 @@ def sort(numberList):
 
             minimumIndex = i
             print("\nMinimum index: " + str(minimumIndex))
+            minIndexLabel.configure(text="Minimum Index: " + str(minimumIndex))
 
             for l in range(i + 1, len(numberList)):
                 if numberList[minimumIndex] > numberList[l]:
                     minimumIndex = l
             print(numberArray)
             Swap.startSwap(numberList, i, minimumIndex)
-            e.config(text=str(numberArray))
+            valueArrayLabel.config(text=str(numberArray))
             root.update()
             root.after(1000)
-            e.config(text=str(numberArray))
+            valueArrayLabel.config(text=str(numberArray))
     else:
         print "Quicksort"
 
@@ -69,9 +73,11 @@ sortingCombo = OptionMenu(root, dropVar, "Selection", "Quicksort", command=lambd
 dropVar.set("Select")
 sortingCombo.grid(row=0, column=1, sticky="ew")
 
-e = Label(root, text="[Empty]")
-e.grid(row=2, column=1)
+valueArrayLabel = Label(root, text="[Empty]")
+valueArrayLabel.grid(row=3, column=1)
 
+minIndexLabel = Label(root, text="Minimum Index: none")
+minIndexLabel.grid(row=2, column=1)
 
 
 arrayListLabel = Label(root, text="Value")
@@ -84,10 +90,13 @@ arrayListButton = Button(root, text="Enter", command=lambda: arrayList(arrayList
 arrayListButton.grid(row=1, column=3)
 
 sortButton = Button(root, text="Sort", command=lambda: sort(numberArray), state="disable")
-sortButton.grid(row=3, column=1)
+sortButton.grid(row=4, column=1)
 
 randomButton = Button(root, text="Randomise", command=lambda: randomiser())
-randomButton.grid(row=4, column=1)
+randomButton.grid(row=5, column=1)
+
+arrayListButton = Button(root, text="Reset", command=lambda: reset())
+arrayListButton.grid(row=5, column=3)
 
 
 
